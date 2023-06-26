54a70d4c3a0fa1a3a9f48a3701b54958a3af09b4
Nguyen Dac Phuong <dacphuongotp@gmail.com>
2023-06-27 01:03:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Login from "./components/auths/Login";
+import HomePage from "./controllers/HomePage";
+import ChatPage from "./controllers/ChatPage";
+import OneToOne from "./components/chat-rooms/OneToOne";
+import AuthProvider from "./contexts/AuthProvider";
+// import AppProvider from './Context/AppProvider';
+// import AddRoomModal from './components/Modals/AddRoomModal';
+// import InviteMemberModal from './components/Modals/InviteMemberModal';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter><AuthProvider>
+      
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<ChatPage />} />
+            <Route path="/chat" element={<OneToOne />} />
+            
+            <Route element={<Login />} path="signin" />
+          </Route>
+        </Routes>
+      
+    </AuthProvider></BrowserRouter>
   );
 }
 
